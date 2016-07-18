c29bd4cfdd7a8c21880e8fe50cc0e7fa90ea7a39
Jeremy Fisher <jeremy@rentawebgeek.com>
2016-07-17 20:56:57 -0500

    Configuring for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'levis',
+  packages = ['levis'],
+  version = '0.3.0',
+  description = 'A toolkit for genetic algorithms in Python.',
+  author = 'Jeremy Fisher',
+  author_email = 'jeremy@rentawebgeek.com',
+  url = 'https://github.com/rawg/levis',
+  download_url = 'https://github.com/rawg/levis/archive/v0.3.0.tar.gz',
+  keywords = ['genetic', 'evolutionary', 'optimization', 'combinatorial', 'search'],
+  classifiers = [],
+)
