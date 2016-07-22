2b9739d321379a5dc1b3ffb5b5e05615feb91851
Jeremy Fisher <jeremy@rentawebgeek.com>
2016-07-22 08:31:22 -0500

    Removing unused dependency

--- tests/test_base.py
+++ tests/test_base.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import
 import unittest
 
 from . import harness
-#import harness
 
 
 class GeneticAlgorithmTestCase(harness.BaseGATestCase):
--- tests/test_behavior.py
+++ tests/test_behavior.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import
 
 import unittest
 
-from . import context
 from . import harness
 
 from levis.behavior import *
